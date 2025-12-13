4be77263906647db6ed8f638b7c0a3c19b780ec6
Mateusz Smuga <mateusz@smuga.pl>
2025-12-13 01:07:38 +0100

    core debug: fixed junit issue

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,10 +2,21 @@ plugins {
     java
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
+
+repositories {
+    mavenCentral()
+}
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.0")
+}
+
 
 tasks.test {
     useJUnitPlatform()
