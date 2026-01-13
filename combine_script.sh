df76f82330c91718109709b5b58c65e068c76557
Mateusz Smuga <mateusz@smuga.pl>
2026-01-13 11:51:09 +0100

    small fix

--- combine_script.sh
+++
@@ -1,7 +0,0 @@
-find . -path './.git' -prune -o -type f -print | while read -r file; do
-    echo "========================================" >> ../combined.txt
-    echo "FILE: $file" >> ../combined.txt
-    echo "========================================" >> ../combined.txt
-    cat "$file" >> ../combined.txt
-    echo -e "\n" >> ../combined.txt
-done
